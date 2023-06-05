86d291ea56a7652139326d3d945d721a8706eba3
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-06-05 13:30:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/93ac380bc3669ecc32750.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/0600fb448ace2fab758fd.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/0e5c3cc77e86b36a14151.jpg",
+    "https://telegra.ph/file/390cf2b464e9f21ecaf89.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/0e5c3cc77e86b36a14151.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/390cf2b464e9f21ecaf89.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/93ac380bc3669ecc32750.jpg"
+            START_IMG_URL = "https://telegra.ph/file/0600fb448ace2fab758fd.jpg"
